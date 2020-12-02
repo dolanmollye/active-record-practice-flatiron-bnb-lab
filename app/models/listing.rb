f905a76e9ad69ad7645d7c256f64844562b5780d
Molly <dolanmollye@gmail.com>
2020-12-01 18:17:28 -0800

    Thank you lucas

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,8 +1,9 @@
 class Listing < ActiveRecord::Base
     belongs_to :neighborhood
-    belongs_to :host, :class_name => "User", foreign_key: "host_id"
+    belongs_to :host, :class_name => User
     has_many :reservations
-    has_many :users, through: :reservations
-    has_many :reviews, :through => :reservations
-    has_many :guests, :through => :reservations
+    # has_many :users, through: :reservations
+    # has_many :reviews, :through => :reservations
+    has_many :guests, :class_name => User, through: :reservations
+    has_many :reviews, through: :guests
 end
